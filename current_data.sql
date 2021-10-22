005c9210129826e0b025331df48af8f27f0d7fe1
AnthonyStevens1 <astevens600@gmail.com>
2021-10-21 20:11:47 -0400

    Uploads to pgadmin

---
+++ current_data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE current_data(
+	fips FLOAT,
+	country VARCHAR(255),
+	state VARCHAR(255),
+	county VARCHAR (255),
+	population INT,
+	transmission_level INT,
+	last_updated DATE
+);
---
+++ daily.sql
@@ -0,0 +1,9 @@
+CREATE TABLE daily(
+	date DATE,
+	county VARCHAR(255),
+	state VARCHAR (255),
+	fips FLOAT,
+	cases INT,
+	deaths FLOAT
+
+);
